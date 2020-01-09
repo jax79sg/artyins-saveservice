9763ee64ccf400ffc2cd2de6df60389df06156ae
jax79sg <jax79sg@yahoo.com.sg>
2020-01-09 16:39:18 +0800

    fix test case order

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from time import sleep
 import json
 class TestModels(unittest.TestCase):
 
-    def test_sqlsavercreatereport(self):
+    def test_sqlsaver1createreport(self):
         sleep(15)
         print("Running SQLSaver create report")
         from savers.sqlsaver import SQLSaver
@@ -12,16 +12,20 @@ class TestModels(unittest.TestCase):
         createjsonobj = json.load(createData)
         saver.create(createjsonobj)
 
-    def test_sqlsavercreateingest(self):
+    def test_sqlsaver2createingest(self):
         sleep(15)
-        print("Running SQLSaver create ingest")
+        print("Running SQLSaver create report")
         from savers.sqlsaver import SQLSaver
         saver = SQLSaver()
+        createData=open('create_report.json','r')
+        createjsonobj = json.load(createData)
+        saver.create(createjsonobj)
+        print("Running SQLSaver create ingest")
         createData=open('create_ingest.json','r')
         createjsonobj = json.load(createData)
         saver.create(createjsonobj)
 
-    def test_sqlsaverupdateingest(self):
+    def test_sqlsaver3updateingest(self):
         sleep(15)
         print("Running SQLSaver update ingest")
         from savers.sqlsaver import SQLSaver
